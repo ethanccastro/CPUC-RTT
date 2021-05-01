1e335e25d3414a78c10660cbdb684d96c1c079ca
Jennifer-464 <jenniferlopezh0@gmail.com>
2021-04-30 21:26:57 -0700

    Added bar graph on upload/download to the main homepage

--- static/js/graphTransition.js
+++ static/js/graphTransition.js
@@ -1,5 +1,7 @@
 
-/// WEST GRAPH FUNCTIONS ///
+/////       RTT FUNCTION        /////
+
+/// WEST RTT GRAPH FUNCTIONS ///
 function westRttTcpTenPercentFunc() {
     $('#10Wgraph').show(1100, "linear");
     $('#20Wgraph').hide(700, "linear");
@@ -24,7 +26,7 @@ function westRttTcpFiftyPercentFunc() {
     $('.col h6').text('This is a work in progress.');
 }
 
-/// EAST GRAPH FUNCTIONS ///
+/// EAST RTT GRAPH FUNCTIONS ///
 function eastRttTcpTenPercentFunc() {
     $('#10Egraph').show(1100, "linear");
     $('#20Egraph').hide(700, "linear");
@@ -49,7 +51,68 @@ function eastRttTcpFiftyPercentFunc() {
     $('.col h6').text('This is a work in progress.');
 }
 
-// West RTT, TCP, Both
+
+
+
+/////       Upload/Download FUNCTION        /////
+
+/// WEST GRAPH FUNCTIONS ///
+function westUpDownTenPercentFunc() {
+    $('#10WUDgraph').show(1100, "linear");
+    $('#20WUDgraph').hide(700, "linear");
+    $('#50WUDgraph').hide(700, "linear");
+    $('.col h4').text(' Top 10% West Upload/Download Data ');
+    $('.col h6').text('In a time of crisis, ask yourself, what will Napoleon do? He would probably dip his croissant in coffee and invade Russia. Not very relevant here because he was never trapped in an underground basement filled with blood thirsty aliens! Thats right ladies and gentlemen, you are screwed');
+}
+
+function westUpDownTwentyPercentFunc() {
+    $('#10WUDgraph').hide(700, "linear");
+    $('#20WUDgraph').show(1100, "linear");
+    $('#50WUDgraph').hide(700, "linear");
+    $('.col h4').text(' Top 20% West Upload/Download Data ');
+    $('.col h6').text('It was a weird concept. Why would I really need to generate a random paragraph? Could I actually learn something from doing so? All these questions were running through her head as she pressed the generate button. To her surprise, she found what she least expected to see.');
+}
+
+function westUpDownFiftyPercentFunc() {
+    $('#10WUDgraph').hide(700, "linear");
+    $('#20WUDgraph').hide(700, "linear");
+    $('#50WUDgraph').show(1100, "linear");
+    $('.col h4').text(' Top 50% West Upload/Download Data ');
+    $('.col h6').text('This is a work in progress.');
+}
+
+/// EAST RTT GRAPH FUNCTIONS ///
+function eastUpDownTenPercentFunc() {
+    $('#10EUDgraph').show(1100, "linear");
+    $('#20EUDgraph').hide(700, "linear");
+    $('#50EUDgraph').hide(700, "linear");
+    $('.col h4').text(' Top 10% East Upload/Download Data ');
+    $('.col h6').text('In a time of crisis, ask yourself, what will Napoleon do? He would probably dip his croissant in coffee and invade Russia. Not very relevant here because he was never trapped in an underground basement filled with blood thirsty aliens! Thats right ladies and gentlemen, you are screwed');
+}
+
+function eastUpDownTwentyPercentFunc() {
+    $('#10EUDgraph').hide(700, "linear");
+    $('#20EUDgraph').show(1100, "linear");
+    $('#50EUDgraph').hide(700, "linear");
+    $('.col h4').text(' Top 20% East Upload/Download Data ');
+    $('.col h6').text('It was a weird concept. Why would I really need to generate a random paragraph? Could I actually learn something from doing so? All these questions were running through her head as she pressed the generate button. To her surprise, she found what she least expected to see.');
+}
+
+function eastUpDownFiftyPercentFunc() {
+    $('#10EUDgraph').hide(700, "linear");
+    $('#20EUDgraph').hide(700, "linear");
+    $('#50EUDgraph').show(1100, "linear");
+    $('.col h4').text(' Top 50% East Upload/Download Data ');
+    $('.col h6').text('This is a work in progress.');
+}
+
+
+
+
+
+/////       RTT FUNCTION        /////
+
+// West RTT Click Function
 $("#buttonWTen").click(function () {
     setTimeout(westRttTcpTenPercentFunc);
 });
@@ -60,7 +123,7 @@ $("#buttonWFifty").click(function () {
     setTimeout(westRttTcpFiftyPercentFunc);
 });
 
-// East RTT, TCP, Both
+// East RTT Click Function
 $("#buttonETen").click(function () {
     setTimeout(eastRttTcpTenPercentFunc);
 });
@@ -71,10 +134,54 @@ $("#buttonEFifty").click(function () {
     setTimeout(eastRttTcpFiftyPercentFunc);
 });
 
+/////       Upload/Download FUNCTION        /////
+
+// West Upload/Download Function
+$("#buttonWUDTen").click(function () {
+    setTimeout(westUpDownTenPercentFunc);
+});
+$("#buttonWUDTwenty").click(function () {
+    setTimeout(westUpDownTwentyPercentFunc);
+});
+$("#buttonWUDFifty").click(function () {
+    setTimeout(westUpDownFiftyPercentFunc);
+});
+
+// East Upload/Download Function
+$("#buttonEUDTen").click(function () {
+    setTimeout(eastUpDownTenPercentFunc);
+});
+$("#buttonEUDTwenty").click(function () {
+    setTimeout(eastUpDownTwentyPercentFunc);
+});
+$("#buttonEUDFifty").click(function () {
+    setTimeout(eastUpDownFiftyPercentFunc);
+});
+
+/// LOADING GRAPHS ///
+function loadWestGraphFunction() {
+    $('#20Wgraph').hide();
+    $('#50Wgraph').hide();
+}
+function loadEastGraphFunction() {
+    $('#20Egraph').hide();
+    $('#50Egraph').hide();
+}
+function loadWestUDGraphFunction() {
+    $('#20WUDgraph').hide();
+    $('#50WUDgraph').hide();
+}
+function loadEastUDGraphFunction() {
+    $('#20EUDgraph').hide();
+    $('#50EUDgraph').hide();
+}
+
 // A $( document ).ready() block.
 $(document).ready(function () {
     setTimeout(loadWestGraphFunction, 2500);
     setTimeout(loadEastGraphFunction, 2500);
+    setTimeout(loadWestUDGraphFunction, 2500);
+    setTimeout(loadEastUDGraphFunction, 2500);
 });
 
 
